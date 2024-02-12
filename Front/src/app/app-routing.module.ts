ea864318869db3858ee99774e5ce64abf557a899
EyaSomaii <eya.somai@esprit.tn>
2024-02-12 11:01:06 +0100

    firt front commit

---
+++ Front/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ListUsersComponent } from 'projects/back-office/src/app/list-users/list-users.component';
+
+const routes: Routes = [  
+
+  { path: 'profile', component: ProfileComponent },
+  { path: 'backoffice', loadChildren: () => import('../../projects/back-office/src/app/app.module').then(m => m.AppModule) }
+
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
